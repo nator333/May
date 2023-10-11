0b56c7a8a1148156a39ef3a16df55bf9c3816382
Nakamata Masahiro <m.nakamata35@gmail.com>
2023-10-10 21:58:53 -0400

    p

---
+++ JavaScript/ObjectOrientedProgramming/index.js
@@ -0,0 +1,12 @@
+const tv = {
+    inch: 34,
+    frameColor: 'silver',
+    isPowerOn: false,
+    modelName: 'Kameyama',
+    resolution: '4K',
+    brand: 'TOSHIBA',
+    application: 'Fire TV',
+};
+
+tv.isPowerOn = true;
+console.log(tv.isPowerOn);
